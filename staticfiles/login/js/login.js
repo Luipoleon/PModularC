a51e6bf77b690584e161075bba5e5c9f615f08e9
Luipoleon <luis.luigis.9999@gmail.com>
2024-02-19 15:18:45 -0600

    Add modal for users that forget their passwords and divide the html in partial files

--- staticfiles/login/js/login.js
+++ staticfiles/login/js/login.js
@@ -1,6 +1,5 @@
 const register = document.querySelectorAll(".form-register");
 const card = document.querySelector(".flip-card");
-const modalPassword = document.querySelector(".modal-message");
 
 Array.from(register).forEach((r) => {
   r.addEventListener("click", (e) => {
