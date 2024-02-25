2db770f169368eb871b38b2ad8953dfd8f3aa9bd
Armando Ruiz Anaya <armandobolt13@gmail.com>
2024-02-25 10:12:16 -0600

    Agregando modificaciones a reportes

--- user/views.py
+++ user/views.py
@@ -5,5 +5,8 @@ from django.shortcuts import render
 def user_index(request):
     return render(request, 'user_index.html', {})
 
-def user_infomration(request):
-    return render(request, 'user_information.html', {})
+def user_reportes(request):
+    return render(request, 'user_reportes.html', {})
+
+def user_reportar(request):
+    return render(request, 'user_reportar.html', {})
