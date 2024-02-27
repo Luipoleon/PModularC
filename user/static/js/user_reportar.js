ba951158e6e48e9b46932fe1e9f992880ca20e6f
Armando Ruiz Anaya <armandobolt13@gmail.com>
2024-02-27 15:29:15 -0600

    Modificaciones a la ruta reportar, agregando js,estilo y mas funciones.

--- user/static/js/user_reportar.js
+++ user/static/js/user_reportar.js
@@ -1,14 +1,67 @@
-
+// Funcion Opciones tipo de edifio
 let TipoEdificio = document.getElementById('TipoEdificio');
-let NombreEdificio = document.getElementById('NombreEdificio');
-let NumeroEdificio = document.getElementById('NumeroEdificio');
-
 TipoEdificio.addEventListener('change', function() {
     if (TipoEdificio.value == 'Academico') {
         NombreEdificio.hidden = false;
         NumeroEdificio.hidden = false;
-    } else {
+        TipoBaño.hidden = true;
+        EdificioBaño.hidden = true;
+        PlantaBaño.hidden = true;
+        TipoAreasComunes.hidden = true;
+        UAreaVerde.hidden = true;
+        UAreaComedor.hidden = true;
+        UAreaEstacionamiento.hidden = true;
+    }else 
+    if (TipoEdificio.value == 'Baños') {
         NombreEdificio.hidden = true;
         NumeroEdificio.hidden = true;
+        TipoBaño.hidden = false;
+        EdificioBaño.hidden = false;
+        PlantaBaño.hidden = false;
+        TipoAreasComunes.hidden = true;
+        UAreaVerde.hidden = true;
+        UAreaComedor.hidden = true;
+        UAreaEstacionamiento.hidden = true;
+
+    }else
+    if (TipoEdificio.value == 'AreasComunes') {
+        NombreEdificio.hidden = true;
+        NumeroEdificio.hidden = true;
+        TipoBaño.hidden = true;
+        EdificioBaño.hidden = true;
+        PlantaBaño.hidden = true;
+        TipoAreasComunes.hidden = false;
+        document.getElementById('TipoAreaComun').addEventListener('change', function() {
+            if (TipoAreaComun.value == 'AreaVerde') {
+                UAreaVerde.hidden = false;
+                UAreaComedor.hidden = true;
+                UAreaEstacionamiento.hidden = true;
+            }else if (TipoAreaComun.value == 'Comedor') {
+                UAreaVerde.hidden = true;
+                UAreaComedor.hidden = false;
+                UAreaEstacionamiento.hidden = true;
+
+            }else if (TipoAreaComun.value == 'Estacionamiento') {
+                UAreaVerde.hidden = true;
+                UAreaComedor.hidden = true;
+                UAreaEstacionamiento.hidden = false;
+                
+            }else{
+                UAreaVerde.hidden = true;
+                UAreaComedor.hidden = true;
+                UAreaEstacionamiento.hidden = true;
+            }
+        });
+    }else {
+        NombreEdificio.hidden = true;
+        NumeroEdificio.hidden = true;
+        TipoBaño.hidden = true;
+        EdificioBaño.hidden = true;
+        PlantaBaño.hidden = true;
+        TipoAreasComunes.hidden = true;
+        UAreaVerde.hidden = true;
+        UAreaComedor.hidden = true;
+        UAreaEstacionamiento.hidden = true;
+        
     }
 });
