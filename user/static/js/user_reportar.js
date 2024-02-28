43aab7b943dbea8ff6f7283afb279d590e3701cd
Armando Ruiz Anaya <armandobolt13@gmail.com>
2024-02-27 20:31:28 -0600

    Terminando reportes, los form

--- user/static/js/user_reportar.js
+++ user/static/js/user_reportar.js
@@ -1,36 +1,40 @@
 // Funcion Opciones tipo de edifio
 let TipoEdificio = document.getElementById('TipoEdificio');
+
+function hideAllElements() {
+    NombreEdificio.hidden = true;
+    NumeroEdificio.hidden = true;
+    TipoBaño.hidden = true;
+    EdificioBaño.hidden = true;
+    PlantaBaño.hidden = true;
+    TipoAreasComunes.hidden = true;
+    UAreaVerde.hidden = true;
+    UAreaComedor.hidden = true;
+    UAreaEstacionamiento.hidden = true;
+    TipoDepartamento.hidden = true;
+    UAdministrativos.hidden = true;
+    UCubiculo.hidden = true;
+    UBiblioteca.hidden = true;
+    UCoordinacion.hidden = true;
+  }
+
+
 TipoEdificio.addEventListener('change', function() {
     if (TipoEdificio.value == 'Academico') {
+        hideAllElements();
         NombreEdificio.hidden = false;
         NumeroEdificio.hidden = false;
-        TipoBaño.hidden = true;
-        EdificioBaño.hidden = true;
-        PlantaBaño.hidden = true;
-        TipoAreasComunes.hidden = true;
-        UAreaVerde.hidden = true;
-        UAreaComedor.hidden = true;
-        UAreaEstacionamiento.hidden = true;
     }else 
     if (TipoEdificio.value == 'Baños') {
-        NombreEdificio.hidden = true;
-        NumeroEdificio.hidden = true;
+        hideAllElements();
         TipoBaño.hidden = false;
         EdificioBaño.hidden = false;
         PlantaBaño.hidden = false;
-        TipoAreasComunes.hidden = true;
-        UAreaVerde.hidden = true;
-        UAreaComedor.hidden = true;
-        UAreaEstacionamiento.hidden = true;
-
     }else
     if (TipoEdificio.value == 'AreasComunes') {
-        NombreEdificio.hidden = true;
-        NumeroEdificio.hidden = true;
-        TipoBaño.hidden = true;
-        EdificioBaño.hidden = true;
-        PlantaBaño.hidden = true;
+        hideAllElements();
         TipoAreasComunes.hidden = false;
+
         document.getElementById('TipoAreaComun').addEventListener('change', function() {
             if (TipoAreaComun.value == 'AreaVerde') {
                 UAreaVerde.hidden = false;
@@ -52,16 +56,40 @@ TipoEdificio.addEventListener('change', function() {
                 UAreaEstacionamiento.hidden = true;
             }
         });
-    }else {
-        NombreEdificio.hidden = true;
-        NumeroEdificio.hidden = true;
-        TipoBaño.hidden = true;
-        EdificioBaño.hidden = true;
-        PlantaBaño.hidden = true;
-        TipoAreasComunes.hidden = true;
-        UAreaVerde.hidden = true;
-        UAreaComedor.hidden = true;
-        UAreaEstacionamiento.hidden = true;
-        
+    }else if (TipoEdificio.value == 'Departamento') {
+        hideAllElements();
+        TipoDepartamento.hidden = false;
+        document.getElementById('TipoDepartamentos').addEventListener('change', function() {
+            if (TipoDepartamentos.value == 'Administrativos') {
+                UAdministrativos.hidden = false;
+                UCubiculo.hidden = true;
+                UBiblioteca.hidden = true;
+                UCoordinacion.hidden = true;
+            }else if (TipoDepartamentos.value == 'Cubiculos') {
+                UAdministrativos.hidden = true;
+                UCubiculo.hidden = false;
+                UBiblioteca.hidden = true;
+                UCoordinacion.hidden = true;
+            }else if (TipoDepartamentos.value == 'Biblioteca') {
+                UAdministrativos.hidden = true;
+                UCubiculo.hidden = true;
+                UBiblioteca.hidden = false;
+                UCoordinacion.hidden = true;
+            }else if (TipoDepartamentos.value == 'Coordinacion') {
+                UAdministrativos.hidden = true;
+                UCubiculo.hidden = true;
+                UBiblioteca.hidden = true;
+                UCoordinacion.hidden = false;
+            }else{
+                UAdministrativos.hidden = true;
+                UCubiculo.hidden = true;
+                UBiblioteca.hidden = true;
+                UCoordinacion.hidden = true;
+            }
+        });
+    }
+    
+    else {
+        hideAllElements();
     }
 });
