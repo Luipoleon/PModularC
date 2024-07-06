1123e4b42343463247598c7c0aff3200dad38f38
Armando Ruiz Anaya <armandobolt13@gmail.com>
2024-07-05 19:40:01 -0600

    permisos

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-hdpk0^d%-jg!*n%&9oh_2@-qoph(+cy53!2uj+bxa(m&70%93&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'mantenimientocucei.azurewebsites.net', 'localhost']
+ALLOWED_HOSTS = ['*', 'cuceimantaint.azurewebsites.net', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ["https://mantenimientocucei.azurewebsites.net", "http://mantenimientocucei.azurewebsites.net"]
+CSRF_TRUSTED_ORIGINS = ["https://cuceimantaint.azurewebsites.net"]
 
 
 
